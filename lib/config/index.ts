15acc14534e989cdc41f39431e8ad13b0cdd21ff
Dinh-Van Colomban <van.colomban@gmail.com>
2024-05-12 16:44:10 +0200

    feat(config): adds endpoints in configs

---
+++ lib/config/index.ts
@@ -0,0 +1,8 @@
+export const Config = {
+  endpoint: {
+    /** Production endpoint */
+    Production: 'https://api.trakt.tv',
+    /** Staging endpoint */
+    Staging: 'https://api-staging.trakt.tv',
+  },
+};
--- lib/mocks/tratk-settings.mock.ts
+++ lib/mocks/tratk-settings.mock.ts
@@ -1,10 +1,12 @@
 import type { TraktClientSettings } from '~/models/trakt-client.model';
 
+import { Config } from '~/config';
+
 export const traktClientSettingsMock: TraktClientSettings = {
   client_id: 'client_id',
   client_secret: 'client_secret',
   redirect_uri: 'chrome-extension://redirect_uri/views/options/index.html',
-  endpoint: 'https://api.trakt.tv',
+  endpoint: Config.endpoint.Production,
 
   useragent: 'my-user-agent',
 };
