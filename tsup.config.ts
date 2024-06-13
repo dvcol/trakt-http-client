a02499e13bf698824fe89f07b6cc221c437a3db0
Dinh-Van Colomban <van.colomban@gmail.com>
2024-06-13 21:29:04 +0200

    fix(build): exclude spec files from published dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['lib', '!lib/**/*.test.ts', '!lib/mocks'],
+  entry: ['lib', '!lib/**/*.test.ts', '!lib/**/*.spec.ts', '!lib/mocks'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
